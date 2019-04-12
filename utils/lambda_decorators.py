e7b8618c0152182b72d12d7267db9f8788467d28
Matthew Amos <mamos@scottlogic.com>
2019-04-12 16:09:37 +0100

    Error handling decorator

--- utils/lambda_decorators.py
+++ utils/lambda_decorators.py
@@ -2,12 +2,24 @@ from functools import wraps
 
 
 def ssm_parameters(ssm_client, *param_names):
-    def ssm_p_decorator(handler):
+    def decorator(handler):
         @wraps(handler)
-        def ssm_wrapper(event, context):
+        def wrapper(event, context):
             ssm_params = ssm_client.get_parameters(Names=[*param_names])
             event['ssm_params'] = {p['Name']: p['Value'] for p in ssm_params}
             return handler(event, context)
-        return ssm_wrapper
-    return ssm_p_decorator
+        return wrapper
+    return decorator
 
+
+def suppress_exceptions(return_val):
+    def decorator(handler):
+        @wraps(handler)
+        def wrapper(event, context):
+            try:
+                return handler(event, context)
+            except Exception as e:
+                print(e)
+                return return_val
+        return wrapper
+    return decorator
