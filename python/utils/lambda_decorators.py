13cf0689eafe277e472a94b3233f57af05fde486
Matthew Amos <35695811+blackmamo@users.noreply.github.com>
2019-04-16 12:21:29 +0100

    Added tests and fixed implementations (#5)
    Added tests

--- python/utils/lambda_decorators.py
+++ python/utils/lambda_decorators.py
@@ -1,18 +1,21 @@
 from functools import wraps
+import traceback
+from utils.json_serialisation import dumps
 
 
 def ssm_parameters(ssm_client, *param_names):
     def decorator(handler):
         @wraps(handler)
         def wrapper(event, context):
-            ssm_params = ssm_client.get_parameters(Names=[*param_names])
+            ssm_params = ssm_client.get_parameters(Names=[*param_names])['Parameters']
+            print(f"Retrieved SSM Parameters {dumps(ssm_params)}")
             event['ssm_params'] = {p['Name']: p['Value'] for p in ssm_params}
             return handler(event, context)
         return wrapper
     return decorator
 
 
-def suppress_exceptions(return_val):
+def suppress_exceptions(return_value):
     def decorator(handler):
         @wraps(handler)
         def wrapper(event, context):
@@ -20,6 +23,10 @@ def suppress_exceptions(return_val):
                 return handler(event, context)
             except Exception as e:
                 print(e)
-                return return_val
+                traceback.print_exc()
+                if isinstance(return_value, Exception):
+                    raise return_value from None
+                else:
+                    return return_value
         return wrapper
     return decorator
---
+++ tests/test_decorators.py
@@ -0,0 +1,51 @@
+import pytest
+from utils import lambda_decorators
+from unittest.mock import MagicMock
+
+
+@pytest.mark.unit
+def test_ssm_params():
+    called = False
+    ssm_mock = MagicMock()
+    ssm_mock.get_parameters.return_value = {
+        'Parameters':
+        [
+            {
+                "Name": "/sec-an/mamos/ecs/cluster",
+                "Type": "StringList",
+                "Value": "arn:aws:ecs:eu-west-2:447213725459:cluster/mamos-sec-an-scanning-cluster",
+                "Version": 1,
+                "LastModifiedDate": "2019-04-08T13:01:12.317000+00:00",
+                "ARN": "arn:aws:ssm:eu-west-2:447213725459:parameter/sec-an/mamos/ecs/cluster"}
+        ]
+    }
+
+    @lambda_decorators.ssm_parameters(ssm_mock, "/param/name")
+    def foo(event, _):
+        nonlocal called
+        called = True
+        assert event['ssm_params'] == {
+            '/sec-an/mamos/ecs/cluster': 'arn:aws:ecs:eu-west-2:447213725459:cluster/mamos-sec-an-scanning-cluster'
+        }
+
+    foo({}, None)
+    assert called
+
+
+@pytest.mark.unit
+def test_suppress_exceptions_throw_exceptional_return():
+    @lambda_decorators.suppress_exceptions(Exception('Rethrow'))
+    def foo(event, _):
+        raise Exception('Original')
+
+    with pytest.raises(Exception, match="Rethrow"):
+        foo({}, None)
+
+
+@pytest.mark.unit
+def test_suppress_exceptions_return_value():
+    @lambda_decorators.suppress_exceptions(5)
+    def foo(event, _):
+        raise Exception('Original')
+
+    assert foo({}, None) == 5
