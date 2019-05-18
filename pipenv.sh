3b133fce5e6df0a035deddbe51a2ae312720862e
Phil Rogers <progers@scottlogic.com>
2019-05-18 16:26:20 +0100

    pipenv.sh need a #! at the start, also reference local file in setup

--- pipenv.sh
+++ pipenv.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 cd infrastructure
 terraform init -backend-config "bucket=$1-terraform-state"
 terraform workspace new $2 || terraform workspace select $2
--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ fi
 
 export PIPENV_VENV_IN_PROJECT=true
 pipenv install --dev
-pipenv run pipenv.sh $1 $2
+pipenv run ./pipenv.sh $1 $2
 
 wait
