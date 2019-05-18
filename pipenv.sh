c2cf44a26b2532ece00c619edd01467dbdc19fe8
Phil Rogers <progers@scottlogic.com>
2019-05-18 16:21:12 +0100

    macos compatibility, destroy script added

--- pipenv.sh
+++ pipenv.sh
@@ -1,6 +1,6 @@
 cd infrastructure
 terraform init -backend-config "bucket=$1-terraform-state"
 terraform workspace new $2 || terraform workspace select $2
-terraform apply -auto-approve -input=true
+terraform apply -auto-approve -input=true -var app_name=$1
 # pause in case the user is watching output
 sleep 5
