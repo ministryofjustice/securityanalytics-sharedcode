a430b2880fcefed47e022581af667aef69138183
Matthew Amos <mamos@scottlogic.com>
2019-05-20 14:00:43 +0100

    Oops, shouldn't have this hardcoded now we expect the user to have setup their env themselves

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -9,7 +9,6 @@ terraform {
     dynamodb_table = "sec-an-terraform-locks"
     key            = "shared_code/terraform.tfstate"
     region         = "eu-west-2"                     # london
-    profile        = "sec-an"
   }
 }
 
