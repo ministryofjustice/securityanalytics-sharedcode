6857647cc4e8096b68d71539935ebcce327d3e99
Matthew Amos <mamos@scottlogic.com>
2019-05-20 10:27:22 +0100

    Formatting

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -37,6 +37,7 @@ variable "known_deployment_stages" {
 
 provider "aws" {
   region = "${var.aws_region}"
+
   # N.B. To support all authentication use cases, we expect the local environment variables to provide auth details.
   allowed_account_ids = ["${var.account_id}"]
 }
