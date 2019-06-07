12db2fb38cfb55224971dd985305dc8aefb7695c
Matthew Amos <mamos@scottlogic.com>
2019-06-07 13:38:19 +0100

    Moved reporting of dead letters to elastic from here to shared infra

--- python/dlq_recorder/dlq_recorder.py
+++ python/dlq_recorder/dlq_recorder.py
@@ -2,8 +2,7 @@ import os
 import io
 import aioboto3
 import time
-from utils.json_serialisation import dumps
-from utils.lambda_decorators import async_handler, ssm_parameters
+from utils.lambda_decorators import async_handler
 import tarfile
 from collections import namedtuple
 from asyncio import run, gather
@@ -17,28 +16,17 @@ bucket = os.environ["S3_BUCKET"]
 prefix = os.environ["S3_KEY_PREFIX"]
 dlq_name = os.environ["DLQ_NAME"]
 
-ssm_client = aioboto3.client("ssm", region_name=region)
 s3_client = aioboto3.client("s3", region_name=region)
-sqs_client = aioboto3.client("sqs", region_name=region)
 
 
-ssm_prefix = f"/{app_name}/{stage}"
-ES_SQS = f"{ssm_prefix}/analytics/elastic/ingest_queue/id"
-
-
-@ssm_parameters(
-    ssm_client,
-    ES_SQS
-)
 @async_handler()
 async def save_dead_letter(event, _):
-    es_queue = event['ssm_params'][ES_SQS]
     writes = []
     for record in event["Records"]:
         meta_data = {}
         attrs = record["attributes"]
         sent_time = attrs["SentTimestamp"]
-        msg_id = record["messageId"]
+        meta_data["messageId"] = msg_id = record["messageId"]
         msg_attrs = record["messageAttributes"]
 
         # updated in this order so that the attrs from the attributes beat the user supplied
@@ -50,35 +38,22 @@ async def save_dead_letter(event, _):
             print(f"WARNING duplicate fields in both messageAttributes and attributes. Attributes' take precedence. {duplicate_keys}")
         meta_data.update(attrs)
 
+        meta_data["DeadLetterQueueName"] = bucket
+        meta_data["DeadMessageBucket"] = bucket
+        meta_data["DeadMessageKey"] = dead_message_key
+
         archive_bytes = _create_archive_bytes(record)
         dead_message_key = f"{prefix}/{msg_id}-{sent_time}.tar.gz"
 
-        elastic_stats = {}
-        elastic_stats.update(meta_data)
-        elastic_stats["DeadMessageBucket"] = bucket
-        elastic_stats["DeadMessageKey"] = dead_message_key
-        elastic_key = {
-            "NonTemporalKey": msg_id,
-            # TODO rename scan end time in elastic ingestor to more general name
-            "ScanEndTime": sent_time
-        }
-
         print(f"Dumping failed event {msg_id}")
-
-        writes += [
+        writes.append(
             s3_client.put_object(
                 Body=archive_bytes,
                 Bucket=bucket,
                 Key=dead_message_key,
                 Metadata=meta_data
-            ),
-            sqs_client.send_message(
-                QueueUrl=es_queue,
-                Subject=f"dead_letters:{dlq_name}",
-                MessageBody=dumps(elastic_stats),
-                MessageAttributes=elastic_key
             )
-        ]
+        )
     await gather(*writes)
 
 
