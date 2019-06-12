8b1d06c3768faa7fa1c5e4292d74ce4a7f7e6793
Phil Rogers <47106782+progers-SL@users.noreply.github.com>
2019-06-12 13:30:42 +0100

    script changes (#30)

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-if [ $# -ne 2 ]
-then
+if [ $# -ne 2 ]; then
     echo "Syntax: setup.sh <app_name> <tf_workspace>"
     sleep 30
     exit
 fi
 
+echo "** Running setup.sh in $PWD (app_name=$1, workspace=$2) **"
 export PIPENV_VENV_IN_PROJECT=true
 pipenv install --dev --clear
 
 # since the terraform step uses python code, it requires we run in an activated venv
-pipenv run `pwd`/terraform.sh $1 $2
+pipenv run $(pwd)/terraform.sh $1 $2
 
 wait
