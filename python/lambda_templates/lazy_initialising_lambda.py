514307c19092967cf2f67c458273c38917c20a6b
Matthew Amos <mamos@scottlogic.com>
2019-07-03 16:14:20 +0100

    New base classes to use elsewhere

---
+++ python/lambda_templates/lazy_initialising_lambda.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+import os
+import aioboto3
+from utils.lambda_decorators import ssm_parameters, async_handler
+
+
+class LazyInitLambda(ABC):
+    def __init__(self, ssm_params_to_load):
+        ABC.__init__(self)
+        self.region = os.environ["REGION"]
+        self.stage = os.environ["STAGE"]
+        self.app_name = os.environ["APP_NAME"]
+        self._ssm_prefix = f"/{self.app_name}/{self.stage}"
+        self._ssm_params_to_load = [f"${self.ssm_prefix}/${x}" for x in ssm_params_to_load]
+
+        self.event = None
+        self.context = None
+        self.initialised = False
+        self.ssm_client = None
+        self.ssm_params = None
+
+    # Overriding this method allows subsclasses to initialise e.g. aws clients
+    @abstractmethod
+    async def _initialise(self):
+        pass
+
+    # This is how the implementor handles the event
+    @abstractmethod
+    async def _invoke(self, event, context):
+        pass
+
+    # Other ssm params can be accessed with this method, uses relative name e.g.
+    # use "/lambda/layers/utils/arn", instead of "/sec-an/dev/lambda/layers/utils/arn"
+    def get_ssm_param(self, relative_name):
+        if not relative_name.starts_with("/"):
+            relative_name = f"/{relative_name}"
+        return self.event.ssm_params[f"{self._ssm_prefix}{relative_name}"]
+
+    @async_handler
+    async def invoke(self, event, context):
+        self.context = context
+        self.event = event
+        await self._ensure_initialised()
+
+        @ssm_parameters(self.ssm_params, self._ssm_params_to_load)
+        async def handle_event_with_params(_event, _context):
+            return await self._invoke(event, context)
+
+    async def _ensure_initialised(self):
+        if not self.initialised:
+            self.initialised = True
+            self.ssm_client = aioboto3.client("ssm", region_name=self.region)
+            # call the subclasses' initialisers
+            await self._initialise()
