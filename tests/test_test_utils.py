744a9e801f4fc1ed60183e8d1a844482f08f5c2f
Matthew Amos <mamos@scottlogic.com>
2019-07-04 18:55:37 +0100

    Fix the tests

--- tests/test_test_utils.py
+++ tests/test_test_utils.py
@@ -1,24 +1,25 @@
 from test_utils.test_utils import serialise_mocks, resetting_mocks
 from utils.json_serialisation import dumps
 import pytest
-from unittest import mock
+from unittest.mock import MagicMock, patch
+import os
 
 
 @pytest.mark.unit
 def test_cant_serialise_mock_normally():
     with pytest.raises(TypeError):
-        dumps(mock.MagicMock())
+        dumps(MagicMock())
 
 
 @pytest.mark.unit
 @serialise_mocks()
 def test_can_serialise_mock_with_decorator():
-    assert isinstance(dumps(mock.MagicMock()), str)
+    assert isinstance(dumps(MagicMock()), str)
 
 
 @pytest.mark.unit
 def test_fail_two_tests_without_resetting():
-    shared_mock = mock.MagicMock()
+    shared_mock = MagicMock()
 
     def one():
         shared_mock()
@@ -35,7 +36,7 @@ def test_fail_two_tests_without_resetting():
 
 @pytest.mark.unit
 def test_pass_two_tests_with_resetting():
-    shared_mock = mock.MagicMock()
+    shared_mock = MagicMock()
 
     @resetting_mocks(shared_mock)
     def one():
@@ -53,7 +54,7 @@ def test_pass_two_tests_with_resetting():
 
 @pytest.mark.unit
 def test_reset_despite_excpetion():
-    shared_mock = mock.MagicMock()
+    shared_mock = MagicMock()
 
     @resetting_mocks(shared_mock)
     def one():
@@ -71,22 +72,15 @@ def test_reset_despite_excpetion():
 
 
 @pytest.mark.unit
-def test_can_combine_serialise_mocks_withOthers():
-    shared_mock = mock.MagicMock()
+def test_can_combine_serialise_mocks_with_others():
+    shared_mock = MagicMock()
+    shared_mock.return_value = 7
 
     @serialise_mocks()
-    @patch.dict(os.environ, TEST_ENV)
+    @patch.dict(os.environ, {})
     @patch("aioboto3.client")
     @patch("utils.json_serialisation.stringify_all")
-    def one():
-        shared_mock()
-        raise Exception()
+    def one(aioboto_mock, stringify_mock):
+        return shared_mock()
 
-    @resetting_mocks(shared_mock)
-    def two():
-        shared_mock()
-        shared_mock.assert_called_once()
-
-    with pytest.raises(Exception):
-        one()
-    two()
+    assert one() == 7
