933036920a44e9e19a8b46e401321cdc6ec6d83e
Matthew Amos <mamos@scottlogic.com>
2019-07-04 09:52:16 +0100

    WIP

--- python/lambda_templates/lazy_initialising_lambda.py
+++ python/lambda_templates/lazy_initialising_lambda.py
@@ -31,7 +31,7 @@ class LazyInitLambda(ABC):
     # Other ssm params can be accessed with this method, uses relative name e.g.
     # use "/lambda/layers/utils/arn", instead of "/sec-an/dev/lambda/layers/utils/arn"
     def get_ssm_param(self, relative_name):
-        if not relative_name.starts_with("/"):
+        if relative_name.starts_with("/"):
             relative_name = f"/{relative_name}"
         return self.event.ssm_params[f"{self._ssm_prefix}{relative_name}"]
 
